e001c014c7cb148b48d286a035dd93ad598e470c
leapon <leapon@gmail.com>
2016-05-21 00:51:24 -0400

    alert component from vue-strap added in App2.vue page

--- src/App2.vue
+++ src/App2.vue
@@ -2,6 +2,25 @@
   <leap-header></leap-header>
     <h1>{{ msg }}</h1>
   <leap-footer></leap-footer>
+  <hr/>
+
+  <alert type="success" >
+  <strong>Well Done!</strong>
+    You successfully read this important alert message.
+  </alert>
+
+  <alert type="info" >
+    <strong>Heads up!</strong> This alert needs your attention, but it's not super important.
+  </alert>
+
+  <alert type="danger" dismissable>
+    <strong>Oh snap!</strong> Change a few things up and try submitting again.
+  </alert>
+
+  <alert type="warning" dismissable>
+    <strong>Warning!</strong> Better check yourself, you're not looking too good.
+  </alert>
+  
 </template>
 
 <script>
@@ -9,6 +28,7 @@ import LeapHeader from './components/LeapHeader.vue'
 import LeapFooter from './components/LeapFooter.vue'
 import CompA from './components/A.vue'
 import CompB from './components/B.vue'
+import { alert } from 'vue-strap'
 
 export default {
   data () {
@@ -20,7 +40,8 @@ export default {
     LeapHeader,
     LeapFooter,
     CompA,
-    CompB
+    CompB,
+    alert
   }
 }
 </script>
