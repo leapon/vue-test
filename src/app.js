febfa108200c5df87c9ab9657880a6396ad4fcd3
leapon <leapon@gmail.com>
2016-05-21 21:10:42 -0400

    auto complete added

--- src/app.js
+++ src/app.js
@@ -9,5 +9,33 @@ new Vue({
       openGithub: function() {
         console.log('in openGithub');
       }
+    },
+    data: {
+      favouriteMonth: '',
+      favouriteSimpson: '',
+      addedExtendedFamily: false,
+      theSimpsons: [
+        {
+          value: 'maggie',
+          text: 'Maggie Simpson',
+          image: 'http://i.imgur.com/eK26qtK.jpg'
+        }, {
+          value: 'lisa',
+          text: 'Lisa Simpson',
+          image: 'http://i.imgur.com/wIb44g9.jpg'
+        }, {
+          value: 'bart',
+          text: 'Bart Simpson',
+          image: 'http://i.imgur.com/XkEz9zg.jpg'
+        }, {
+          value: 'marge',
+          text: 'Marge Simpson',
+          image: 'http://i.imgur.com/MuFcpQ4.jpg'
+        }, {
+          value: 'homer',
+          text: 'Homer Simpson',
+          image: 'http://i.imgur.com/aYPRWX4.jpg'
+        }
+      ]
     }
 });
