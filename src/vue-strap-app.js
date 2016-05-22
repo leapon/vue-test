40f6c202bd1ba2a25d9910d59e8e2de901c2b01c
leapon <leapon@gmail.com>
2016-05-22 00:42:05 -0400

    datepicker component added in vue-strap page

--- src/vue-strap-app.js
+++ src/vue-strap-app.js
@@ -1,10 +1,15 @@
 var vm = new Vue({
   components: {
-    alert: VueStrap.alert
+    alert: VueStrap.alert,
+    datepicker: VueStrap.datepicker
   },
   el: "#strap1",
   data: {
     showRight: false,
-    showTop: false
+    showTop: false,
+    disabled: [],
+    value: 'Oct/06/2015',
+    format: ['MMM/dd/yyyy'],
+    reset: true
   }
 })
