8e74a8f25837db46fa2ef2d8cbabdfc6c554580a
leapon <leapon@gmail.com>
2016-05-22 00:36:12 -0400

    vue-strap page works - alert displayed

--- src/vue-strap-app.js
+++ src/vue-strap-app.js
@@ -1,38 +1,10 @@
-Vue.use(VueStrap);
-
-new Vue({
-    el: '#strap1',
-    components: {
-      accordion: VueStrap.accordion
-    },
-    methods: {
-    },
-    data: {
-      favouriteMonth: '',
-      favouriteSimpson: '',
-      addedExtendedFamily: false,
-      theSimpsons: [
-        {
-          value: 'maggie',
-          text: 'Maggie Simpson',
-          image: 'http://i.imgur.com/eK26qtK.jpg'
-        }, {
-          value: 'lisa',
-          text: 'Lisa Simpson',
-          image: 'http://i.imgur.com/wIb44g9.jpg'
-        }, {
-          value: 'bart',
-          text: 'Bart Simpson',
-          image: 'http://i.imgur.com/XkEz9zg.jpg'
-        }, {
-          value: 'marge',
-          text: 'Marge Simpson',
-          image: 'http://i.imgur.com/MuFcpQ4.jpg'
-        }, {
-          value: 'homer',
-          text: 'Homer Simpson',
-          image: 'http://i.imgur.com/aYPRWX4.jpg'
-        }
-      ]
-    }
-});
+var vm = new Vue({
+  components: {
+    alert: VueStrap.alert
+  },
+  el: "#strap1",
+  data: {
+    showRight: false,
+    showTop: false
+  }
+})
