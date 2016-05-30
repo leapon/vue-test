081c2ec4551f6b9b9b914bc58e7c719d04ace95f
leapon <leapon@gmail.com>
2016-05-30 19:52:39 -0400

    multiple entries added in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,14 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './src/main.js',
+  entry: {
+    build: ['./src/main.js'],
+    explorer: ['./src/explorer.js']
+  },
   output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
-    filename: 'build.js'
+    path: './dist',
+    publicPath: 'dist/',
+    filename: '[name].js'
   },
   resolveLoader: {
     root: path.join(__dirname, 'node_modules'),
