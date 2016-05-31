ec00910e7eea510930b0572ac7a6a4fbd1e9da46
leapon <leapon@gmail.com>
2016-05-31 18:49:27 -0400

    src/vuex folder added with actions and store script; ManualCounter.vue added

---
+++ src/components/ManualCounter.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="counter">
+      Clicked: {{ count }} times
+      <button v-on:click="increment">+</button>
+      <button v-on:click="decrement">-</button>
+    </div>
+</template>
+
+<script>
+    import store from '../vuex/store'
+    import { increment, decrement } from '../vuex/actions';
+    
+    export default {
+        props: ['count'],
+        vuex: {
+            getters: {
+              count: state => state.count
+            },
+            actions: {
+                increment,
+                decrement
+            }
+        }
+      }
+    }
+</script>
+
+<style>
+    .counter {
+        border: 1px solid black;
+        padding: 1rem 2rem;
+        margin: 1rem;
+        display: inline-block;
+    }
+</style>
