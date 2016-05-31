4d823c1b659170fc26540d36102f8a54cf73339d
leapon <leapon@gmail.com>
2016-05-30 20:02:31 -0400

    explorer page - side filter panel added

---
+++ src/FilterPane.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <test-select></test-select>
+    <br/>
+    <test-select2></test-select2>
+    <br/>
+    <test-select2></test-select2>
+    <br/>
+    <test-select2></test-select2>
+    <br/>
+    <test-select2></test-select2>
+  </div>
+</template>
+
+<script>
+import TestSelect from './components/TestSelect.vue'
+import TestSelect2 from './components/TestSelect2.vue'
+
+export default {
+  data () {
+    return {
+      msg: 'Nice Vue'
+    }
+  },
+  components: {
+    TestSelect,
+    TestSelect2
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
--- src/explorer.js
+++ src/explorer.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
-import App from './App.vue'
+import FilterPane from './FilterPane.vue'
 
 new Vue({
-  el: 'body',
-  components: { App }
+  el: '#side_panel',
+  components: { FilterPane }
 })
