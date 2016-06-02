43dd5de51415f492b8e08ce1d70084c9b3a96926
leapon <leapon@gmail.com>
2016-06-02 00:09:51 -0400

    add custom directive in plot page

---
+++ src/components/Barplot.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="barplot-container">
+    <div>label: {{ label }}</div>
+    <div id="barplot1"></div>
+  </div>
+</template>
+
+<script>
+//var plotly = require('plotly.js');
+
+export default {
+  props: ['label'],
+  components: {},
+  data () {
+    return {
+    }
+  },
+  activate: function (done) {
+    console.log('>>> activate Barplot');
+    /*
+    var data = [
+      {
+        "x": [
+          "Female",
+          "Male"
+        ],
+        "y": [
+          17,
+          22
+        ],
+        "type": "bar"
+      }
+    ];
+    var layout = null;
+    Plotly.plot('#barplot1', data, layout, { displayModeBar:false });
+    */
+    done();
+  }
+}
+</script>
+
+<style scoped>
+.barplot-container {
+  padding: 10px;
+  border: solid 1px #f9f9f9;
+  font-size: 1.2em;
+}
+</style>
--- src/plot.js
+++ src/plot.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import store from './vuex/store'
 import SimpleLabel from './components/SimpleLabel.vue'
+import SimpleLabel2 from './components/SimpleLabel2.vue'
+import Barplot from './components/Barplot.vue'
+
+Vue.directive('my-directive', function (newValue, oldValue) {
+  console.log('>>> my-directive update:', newValue, oldValue);
+})
 
 new Vue({
   el: '#plot1',
@@ -8,7 +14,9 @@ new Vue({
   data: {
   },
   components: {
-    SimpleLabel
+    SimpleLabel,
+    SimpleLabel2,
+    Barplot
   }
 })
 
