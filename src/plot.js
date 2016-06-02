52d9479a627f229059e1f83c7094727bc0898d17
leapon <leapon@gmail.com>
2016-06-02 09:18:02 -0400

    barplot is drawn on plot page

--- src/plot.js
+++ src/plot.js
@@ -69,3 +69,73 @@ var vm = new Vue({
   }
 })
 
+
+// barplot directive
+
+Vue.directive('barplot', {
+  twoWay: true,
+  priority: 1000,
+
+  params: ['plotdata'],
+    
+  bind: function () {
+    var self = this
+    console.log('>>> barplot directive bind');
+    
+    var plotData = {
+        "value": {
+            "data": [
+                {
+                    "x": [
+                        "Female",
+                        "Male"
+                    ],
+                    "y": [
+                        17,
+                        22
+                    ],
+                    "type": "bar"
+                }
+            ]
+        }
+    };
+    // draw plot using plotly
+    var data = plotData.value.data;
+    var layout = plotData.layout;
+    Plotly.plot(this.el, data, layout, { displayModeBar:false });
+    
+    /*
+    $(this.el)
+      .select2({
+        data: this.params.options
+      })
+      .on('change', function () {
+        self.set(this.value)
+      })
+    */
+  }
+  /*
+  update: function (value) {
+    $(this.el).val(value).trigger('change')
+  },
+  unbind: function () {
+    $(this.el).off().select2('destroy')
+  }
+  */
+})
+
+
+
+
+var vm = new Vue({
+  el: '#plot_test1',
+  data: {
+    plotvalue: 200,
+    plotdata: [
+      { id: 1, text: 'hello' },
+      { id: 2, text: 'what' },
+      { id: 3, text: 'world' }
+    ]
+  }
+})
+
