339ee9691f15b03f29f3e4a857877f229fb1721a
leapon <leapon@gmail.com>
2016-06-02 07:07:25 -0400

    custom directive for select2 added

--- src/plot.js
+++ src/plot.js
@@ -20,5 +20,40 @@ new Vue({
   }
 })
 
+// directive for applying select2 
+Vue.directive('select', {
+  twoWay: true,
+  priority: 1000,
+
+  params: ['options'],
+    
+  bind: function () {
+    var self = this
+    $(this.el)
+      .select2({
+        data: this.params.options
+      })
+      .on('change', function () {
+        self.set(this.value)
+      })
+  },
+  update: function (value) {
+    $(this.el).val(value).trigger('change')
+  },
+  unbind: function () {
+    $(this.el).off().select2('destroy')
+  }
+})
+
+var vm = new Vue({
+  el: '#el',
+  data: {
+    selected: 1,
+    options: [
+      { id: 1, text: 'hello' },
+      { id: 2, text: 'what' }
+    ]
+  }
+})
 
 
