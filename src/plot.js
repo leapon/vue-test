cc4e812d87631c8f274f596df29d3cbfbb3110b4
leapon <leapon@gmail.com>
2016-06-03 08:31:09 -0400

    added scatter plot example in plot page

--- src/plot.js
+++ src/plot.js
@@ -6,10 +6,16 @@ new Vue({
   el: '#plot1',
   store,
   data: {
-    plotdata1: [{ "x":["A", "B"], "y":[3, 5], "type":"bar" }],
     layout1: null,
+    layout2: { "width":300, "height":300 },
+    layout3: { "width":420, "height":400 },
+    plotdata1: [{ "x":["A", "B"], "y":[3, 5], "type":"bar" }],
     plotdata2: [{ "x":["Apple", "Orange"], "y":[23, 18], "type":"bar" }],
-    layout2: { "width":300, "height":300 }
+    scatter1: [{
+      x: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+      y: [0, 3, 4, 5, 4, 6, 9, 11, 12],
+      type: 'scatter'
+    }]
   },
   components: {
     Barplot
