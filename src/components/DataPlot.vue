31d97ec6c61d6a9696e3ed2d61dc853cb6ad888d
leapon <leapon@gmail.com>
2016-06-05 13:00:03 -0400

    update DataPlot component

---
+++ src/components/DataPlot.vue
@@ -0,0 +1,51 @@
+<template>
+  <p>{{ filter2 }}</p>
+  <hr/>
+  <div v-for="dataitem in dataitems">
+    <p>{{ dataitem.id }} - {{ dataitem.name }}</p>
+  </div>
+  <hr/>
+</template>
+
+<script>
+export default {
+  props: ['dataitems'],
+  data () {
+    return {
+      columns: [
+          'name',
+          'nickname',
+          'email',
+          'birthdate',
+          'gender',
+          '__actions'
+      ]
+    }
+  },
+  computed: {
+    filter2: function() {
+      return JSON.stringify(this.filter) + '-2';
+    },
+    dataurl: function() {
+      return './lib/data/vuetable-example-users.json' + '?a=1&filter=' + JSON.stringify(this.filter)
+    }
+  },
+  vuex: {
+    getters: {
+      filter: state => state.filter
+    }
+  },
+  activate: function (done) {
+    console.log('>>> activate DataPlot');
+    done();
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
--- src/plotmanager.js
+++ src/plotmanager.js
@@ -1,24 +1,12 @@
 import Vue from 'vue'
 import store from './vuex/store'
 import FilterPane from './components/FilterPane.vue'
-import UserTable from './components/UserTable.vue'
+import DataPlot from './components/DataPlot.vue'
 
 var VueResource = require('vue-resource');
 Vue.use(VueResource);
 
-// require or import Vuetable and optional VuetablePagination component
-import Vuetable from './vue-table-components/Vuetable.vue';
-import VuetablePagination from './vue-table-components/VuetablePagination.vue';
-import VuetablePaginationDropdown  from './vue-table-components/VuetablePaginationDropdown.vue';
-import VuetablePaginationBootstrap from './vue-table-components/VuetablePaginationBootstrap.vue';
-
-// register components to Vue
-Vue.component('vuetable', Vuetable);
-Vue.component('vuetable-pagination', VuetablePagination)
-Vue.component('vuetable-pagination-dropdown', VuetablePaginationDropdown)
-Vue.component('vuetable-pagination-bootstrap', VuetablePaginationBootstrap)
-
-console.log('in user manager page');
+console.log('in plot manager page');
 
 var values = [
   { name: 'MD' },
@@ -69,6 +57,6 @@ var vmMain = new Vue({
     ]
   },
   components: {
-    UserTable
+    DataPlot
   }
 });
