391c67bc32a7ae38353d6596acd586d0f3ead732
leapon <leapon@gmail.com>
2016-06-17 22:36:56 -0400

    add buttons in MintForm

--- src/components/MintForm.vue
+++ src/components/MintForm.vue
@@ -2,7 +2,19 @@
   <p>MintForm</p>
   <p>{{ label }}</p>
   <hr/>
-  
+  <div>
+    <mt-button type="default">default</mt-button>
+    <mt-button type="primary">primary</mt-button>
+    <mt-button type="danger">danger</mt-button>
+  </div>
+  <hr/>
+  <div>
+    <mt-button size="small">small</mt-button>
+    <mt-button size="normal">normal</mt-button>
+    <mt-button size="large">large</mt-button>
+  </div>
+  <hr/>
+
 </template>
 
 <script>
