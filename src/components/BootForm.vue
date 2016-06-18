8de1eb8cc5181a01bef60b63620cc52f277de018
leapon <leapon@gmail.com>
2016-06-18 11:33:13 -0400

    add tabset, tab in BootForm

--- src/components/BootForm.vue
+++ src/components/BootForm.vue
@@ -1,14 +1,29 @@
 <template>
   <p>{{ label }}</p>
   <hr/>
-  <br/>
+  <div>
+    <h4>Simple Tabs</h4>
+    <tabset>
+      <tab :header="'Tab 1'">
+        This is Tab 1.
+      </tab>
+      <tab :header="'Tab 2'">
+        This is Tab 2.
+      </tab>
+      <tab :header="'Tab 3'">
+        This is Tab 3.
+      </tab>
+    </tabset>
+    <br/>
+  </div>
 </template>
 
 <script>
+import { tabset, tab  } from 'vueboot'
 
 module.exports = {
   props: ['label', 'value'],
-  components: {  },
+  components: { tabset, tab },
   methods:{}
 }
 </script>
