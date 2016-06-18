5dc6546b3bff07d211b42f63e5709cc5a92a9429
leapon <leapon@gmail.com>
2016-06-18 00:14:24 -0400

    form fields added

--- src/components/MintForm.vue
+++ src/components/MintForm.vue
@@ -9,11 +9,29 @@
   </div>
   <br/>
   <div>
+    <mt-radio
+      title="radio list"
+      :value.sync="value2"
+      :options="['optionA', 'optionB', 'optionC']">
+    </mt-radio>
+  </div>
+  <br/>
+  <div>
+    <mt-field label="username" placeholder="Input username"></mt-field>
+    <mt-field label="email" placeholder="Input email" type="email"></mt-field>
+    <mt-field label="password" placeholder="Input password" type="password"></mt-field>
+    <mt-field label="phone" placeholder="Input tel" type="tel"></mt-field>
+    <mt-field label="website" placeholder="Input website" type="url"></mt-field>
+    <mt-field label="number" placeholder="Input number" type="number"></mt-field>
+    <mt-field label="birthday" placeholder="Input birthday" type="date"></mt-field>
+    <mt-field label="introduction" placeholder="introduction" type="textarea" rows="4"></mt-field>
+  </div>
+  <br/>
+  <div>
     <mt-button type="default">default</mt-button>
     <mt-button type="primary">primary</mt-button>
     <mt-button type="danger">danger</mt-button>
   </div>
-  <hr/>
 </template>
 
 <script>
@@ -22,7 +40,7 @@ import MintUI from 'mint-ui';
 Vue.use(MintUI);
 
 module.exports = {
-  props: ['label', 'value', 'options'],
+  props: ['label', 'value', 'value2', 'options'],
   components: { MintUI },
   methods:{}
 }
