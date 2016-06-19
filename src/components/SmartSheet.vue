b15462a6779cebc8185141fb52afdb8c54027c2e
leapon <leapon@gmail.com>
2016-06-19 00:06:40 -0400

    smartsheet update

--- src/components/SmartSheet.vue
+++ src/components/SmartSheet.vue
@@ -1,57 +1,27 @@
 <template>
-  <p>Smart Sheet</p>
+  <p>{{ label }}</p>
   <table class="table table-bordered table-striped"">
   <tr>
-    <th>Name</th>
-    <th>Value</th>
+    <th v-for="column in columns">{{ column.display }}</th>
   </tr>
   <tr v-for="item in items">
-    <td class="text center" @dblclick="editable($event,'name',item)">
+    <td>
       {{ item.name }}
     </td>
-      <td class="text center" @dblclick="editable($event,'value',item)">
-        {{ item.value }}
-      </td>
+    <td>
+      {{ item.value }}
+    </td>
   </tr>
   </table>
 </template>
 
 <script>
 export default {
-  props: ['label', 'items'],
-  data () {
-    return {
-    }
+  props: ['label', 'columns', 'items'],
+  data: function() {
+    return {};
   },
   methods:{
-    editable:function(e,field,user){
-      var target=e.target;
-      var value=target.innerText;
-      target.innerHTML = "<input type='text' value='" + value + "' id='_editable' style='width:100%;box-sizing:border-box;background:transparent;font-size:13px;color:red;text-align:center'>";
-      var input = document.getElementById('_editable');
-      input.focus();
-      var len = input.value.length;
-
-      if (document.selection) {
-        var sel = input.createTextRange();
-        sel.moveStart('character', len);
-        sel.collapse();
-        sel.select();
-      } else if (typeof input.selectionStart == 'number' && typeof input.selectionEnd == 'number') {
-        input.selectionStart = input.selectionEnd = len;
-      }
-
-      var action = function() {
-        if (value != this.value && this.value != '') {
-          target.innerHTML = this.value;
-          //callback(this.value)
-        } else {
-          target.innerHTML = value;
-        }
-        input.removeEventListener("blur", action, false);
-      };
-      input.addEventListener("blur", action, false);
-    }
   }
 }
 </script>
--- src/smartsheet.js
+++ src/smartsheet.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import store from './vuex/store'
-import SmartSheet from './components/smartsheet.vue'
+import SmartSheet from './components/SmartSheet.vue'
 
 new Vue({
   el: '#sheet1',
   store,
   data: {
     label: 'List of Items',
+    columns: [
+      { name:'name', display:'Name' },
+      { name:'value', display:'Value' }
+    ],
     items: [
       { name:'table', value:20 },
       { name:'bench', value:25 },
